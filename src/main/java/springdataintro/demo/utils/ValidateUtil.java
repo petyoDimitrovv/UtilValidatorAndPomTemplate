4277ee06dcfbe84182ea989f44cdc2bcc37ffe4e
Petyo Dimitrov <78147585+petyoDimitrovv@users.noreply.github.com>
2021-04-27 16:12:26 +0300

    Update ValidateUtil.java

--- src/main/java/springdataintro/demo/utils/ValidateUtil.java
+++ src/main/java/springdataintro/demo/utils/ValidateUtil.java
@@ -8,4 +8,15 @@ public interface ValidateUtil {
     <E> boolean isValid(E entity);
 
     <E> Set<ConstraintViolation<E>> violations(E entity);
+    //TODO: implement validator in config: 
+//     @Bean
+//     public Validator validator(){
+//         return Validation.buildDefaultValidatorFactory().getValidator();
+//     }
+
+//     @Bean
+//     public ValidateUtil validateUtil() {
+//        return new ValidatorUtilImpl(validator());
+//     }
+
 }
